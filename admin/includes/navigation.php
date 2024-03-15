6a658398ab8d38f9766d5c0fdee3133b8f1adb10
radosevic-dr <radosevic.dejan@proton.me>
2024-03-15 19:14:47 +0100

    Added posts table

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -35,10 +35,10 @@
 
       
       <li>
-        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+        <a href="#category"><i class="fa fa-fw fa-wrench"></i> Categories</a>
       </li>
       <li>
-        <a href="javascript:;" data-toggle="collapse" data-target="#posts"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+        <a href="#posts" data-toggle="collapse" data-target="#posts"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
         <ul id="posts" class="collapse">
           <li>
             <a href="#">Add Post</a>
